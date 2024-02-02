2808f89c4397772d58eae6b62035fd047a53a109
Moritz Stellmacher <stellmacher@gmail.com>
2024-02-02 09:12:54 -0300

    Fix Test

--- uniswap-tutorial/1-introduction-to-uniswap/introduction.sol
+++ uniswap-tutorial/1-introduction-to-uniswap/introduction.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract UniswapTutorialTest {
+contract UniswapTutorial {
     // Quiz options
     uint8 public question1 = 0;
     uint8 public question2 = 0;
